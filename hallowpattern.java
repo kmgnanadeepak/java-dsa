d70b2dbfa0b7e7f8ea1d6e71d8db0af73ca24626
kmgnanadeepak <kmgnanadeepak@gmail.com>
2025-12-09 22:07:00 +0530

    3rd dsa question

--- hallowpattern.java
+++ hallowpattern.java
@@ -1,8 +1,13 @@
-public class hallowpattern {
+public class pattern {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("*");
+        int i, j;
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println(" ");
         }
