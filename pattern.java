01a8eb94f105b99f99e1455e0eb7211999f60b3b
kmgnanadeepak <kmgnanadeepak@gmail.com>
2025-12-09 22:02:19 +0530

    first dsa question

--- pattern.java
+++ pattern.java
@@ -1,7 +1,6 @@
 public class pattern {
     public static void main(String[] args) {
-        int n=5;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=5;i++){
             for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
