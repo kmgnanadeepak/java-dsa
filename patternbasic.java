d08e76da544bcc114f37dc0f67a4bd596d2ab0c8
kmgnanadeepak <kmgnanadeepak@gmail.com>
2025-12-30 20:36:45 +0530

    30/12/2025

--- patternbasic.java
+++ patternbasic.java
@@ -38,7 +38,44 @@ public class patternbasic {
         }
     }
 
+    public static void patternjloop(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+     public static void patterniloopreverse(int n){
+          for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void patternjloopreverse(int n){
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void patternfullpyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=i+2;j<=i;j=j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args) {
-        patterniloop(5);
+        patternfullpyramid(8);
     }
 }
