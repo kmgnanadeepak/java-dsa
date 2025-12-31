4bf5b42928aec4aa4ac9c50979967214b0e5072c
kmgnanadeepak <kmgnanadeepak@gmail.com>
2025-12-31 20:52:08 +0530

    31/12/2025

--- two_pointer/maxsubarray.java
+++ two_pointer/maxsubarray.java
@@ -4,7 +4,7 @@ public class maxsubarray {
     public static void maxsub(){
         int arr[]={2,4,6,8,9};
             for(int i=0;i<=arr.length-1;i++){
-                for(int j=i+1;j<=arr.length-1;j++){
+                for(int j=i;j<=arr.length-1;j++){
                     System.out.print("["+arr[i]+","+arr[j]+"]");
                 }
                 System.out.println(" ");
