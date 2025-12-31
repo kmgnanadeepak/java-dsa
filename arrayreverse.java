29c4181dedd80be85d6b18a2bc9bae06133767d9
kmgnanadeepak <kmgnanadeepak@gmail.com>
2025-12-31 11:29:16 +0530

    31/12/2025

--- arrayreverse.java
+++
@@ -1,8 +0,0 @@
-public class arrayreverse {
-    public static void main(String[] args) {
-        int arr[]={2,4,6,8,10};
-        for(int i=arr.length-1;i>=0;i--){
-            System.out.println(arr[i]);
-        }
-    }
-}
