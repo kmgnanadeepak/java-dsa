be72bbdbe7b2486d6e9bc97f95b28e6ca0daa6d6
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-02 22:32:27 +0530

    2/1/2026

---
+++ two_pointer/longest_array_below_sum.java
@@ -0,0 +1,19 @@
+package two_pointer;
+
+public class longest_array_below_sum {
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 1, 7, 10};
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            String subarray = "";
+            for (int j = i; j < arr.length; j++) {
+                sum=sum+arr[j];
+                if (sum > 14) {
+                    break;
+                }
+                subarray = subarray+arr[j] + ",";
+                System.out.println("["+subarray+"]");
+            }
+        }
+    }
+}
