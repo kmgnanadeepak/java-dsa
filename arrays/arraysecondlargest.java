1871a48454c6ba5b23629bffd67b841bbaacf040
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-10 21:11:55 +0530

    2/1/2026

---
+++ arrays/arraysecondlargest.java
@@ -0,0 +1,9 @@
+public class arraysecondlargest {
+    public static void main(String[] args) {
+        int arr[]={1,2,5,7,7};
+        int seclargest=0;
+        for(int i=arr.length-2;i>0;i++){
+            if()
+        }
+    }
+}
