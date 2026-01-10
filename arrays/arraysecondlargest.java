ca5a393717af1e723a15cdc5a880395e897f48c0
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-10 21:18:59 +0530

    2/1/2026

--- arrays/arraysecondlargest.java
+++ arrays/arraysecondlargest.java
@@ -1,9 +1,13 @@
 public class arraysecondlargest {
     public static void main(String[] args) {
         int arr[]={1,2,5,7,7};
-        int seclargest=0;
-        for(int i=arr.length-2;i>0;i++){
-            if()
+        int seclargest=0,largest = arr[arr.length - 1];
+        for(int i=arr.length-1;i>0;i--){
+            if(arr[i]!=largest){
+                seclargest=arr[i];
+                break;
+            }
         }
+        System.out.println(seclargest);
     }
 }
