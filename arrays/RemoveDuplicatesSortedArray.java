8c22d344b23b87cfa2e25a370673d0e6a8532847
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-11 18:56:53 +0530

    2/1/2026

--- arrays/RemoveDuplicatesSortedArray.java
+++ arrays/RemoveDuplicatesSortedArray.java
@@ -1,10 +1,10 @@
 public class RemoveDuplicatesSortedArray{
     public static void main(String[]args){
-        int[]arr={1,1,2,2,2,3,3};
+        int[]arr={1,1,2,2,2,3,3,4};
         int n=arr.length;
         int i=0;
         for(int j=1;j<n;j++){
-            if(arr[i]!=arr[j]){
+            if(arr[j]!=arr[i]){
                 arr[i+1]=arr[j];
                 i++;
             }
