a4f699050cf3ee832b5f4190ca93a162737152d5
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-11 11:50:55 +0530

    2/1/2026

--- arrays/arrayremovedup.java
+++ arrays/arrayremovedup.java
@@ -2,9 +2,12 @@ public class arrayremovedup {
     public static void main(String[] args) {
         int arr[]={1,2,2,3,4,4};
         for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j!=i;j++){
-                arr[i]=arr[j];
+            for(int j=i;j<arr.length;j++){
+               if(arr[j]!=arr[i]){
+                arr[i+1]=arr[j];
+               }
             }
+           System.out.println(arr[i]);
         }
     }
 }
