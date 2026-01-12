7f05b6cf0eb9d496e4cd88990aa1e417c4eccb73
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-12 12:13:41 +0530

    2/1/2026

--- arrays/arrayleftrotate.java
+++ arrays/arrayleftrotate.java
@@ -7,6 +7,6 @@ public class arrayleftrotate {
         }
         arr[arr.length-1]=temp;
         for(int i=0;i<arr.length;i++)
-          System.out.println(arr[i]);
+          System.out.print(arr[i]);
     }
 }
