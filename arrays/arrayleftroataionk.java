f53736dee3635dfe43d0aea04a6035aa80dd9435
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-12 12:36:47 +0530

    2/1/2026

---
+++ arrays/arrayleftroataionk.java
@@ -0,0 +1,22 @@
+public class arrayleftroataionk {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7};
+        int k=3;
+        k=k%arr.length;
+        int temp[]=new int[k];
+        for(int i=0;i<k;i++){
+            temp[i]=arr[i];
+        }
+        for(int i=k;i<arr.length;i++){
+            arr[i-k]=arr[i];
+        }
+        int j=0;
+        for(int i=arr.length-k;i<arr.length;i++){
+            arr[i]=temp[j];
+            j++;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
