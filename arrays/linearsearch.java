cdab70fedeea7c46763b3e5195c3c98f889b00f8
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-12 18:18:54 +0530

    12/1/2026

---
+++ arrays/linearsearch.java
@@ -0,0 +1,11 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int arr[]={2,1,3,4,6};
+        int target=4;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println("target found at : "+i);
+            }
+        }
+    }
+}
