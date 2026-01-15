2bbb68b2d6ede3827a2aee75c4680f134da5b741
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-15 10:35:12 +0530

    15/01/2026

--- arrays/array_missing_element.java
+++ arrays/array_missing_element.java
@@ -1,10 +1,13 @@
 public class array_missing_element {
     public static void main(String[] args) {
-        int arr[]={1,2,3,5};
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i+1]-arr[i]>1){
-                 System.out.println((arr[i]+1)+" is missing");
-            }
+        int arr[]={1,2,3,4,6};
+        int xor1=0;
+        int xor2=0;
+        for(int i=0;i<arr.length;i++){
+            xor1=xor1^(i+1);
+            xor2=xor2^arr[i];
         }
+       int xor3=xor1^xor2;
+       System.out.println(xor3);
     }
 }
