422ff29464020bd525871c00ee148e7bd79d9ef7
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-15 11:47:28 +0530

    15/01/2026

--- arrays/array_once_twice.java
+++ arrays/array_once_twice.java
@@ -1,8 +1,8 @@
 public class array_once_twice {
     public static void main(String[] args) {
         int arr[]={1,1,2,3,3,4,4,5,5};
-        int count=0;
         for(int i=0;i<arr.length;i++){
+            int count=0;
             for(int j=0;j<arr.length;j++){
                 if(arr[i]==arr[j]){
                     count++;
