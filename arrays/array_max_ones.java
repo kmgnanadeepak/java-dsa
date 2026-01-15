3b10df1137b7f306ee696754270e6f99fa9aad6e
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-15 10:34:58 +0530

    15/01/2026

---
+++ arrays/array_max_ones.java
@@ -0,0 +1,19 @@
+public class array_max_ones {
+    public static void main(String[] args) {
+        int arr[]={0,1,1,0,0,1,1,1,0,1};
+        int count=0,max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                count=count+1;
+                max=count;
+                if(arr[i]==0){
+                    count=0;
+                }
+                if(count>max){
+                    max=count;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
