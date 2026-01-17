f4f8303af04aab4ca59f6ba93c9498bc096ae515
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-01-17 21:11:02 +0530

    17/1/2026

--- recursions/print_numbers.java
+++ recursions/print_numbers.java
@@ -1,6 +1,7 @@
 package recursions;
 
 public class print_numbers {
+
     public static void fun(){
         int count=1;
         if(count==11){
@@ -10,7 +11,30 @@ public class print_numbers {
         count++;
         fun();
     }
+
+    
+    static int count1=0;
+    public static void even(){
+        if(count1>20){
+            return;
+        }
+        System.out.println(count1);
+        count1=count1+2;
+        even();
+    }
+
+    static int count2=1;
+    public static void odd(){
+        if(count2>20){
+            return;
+        }
+        System.out.println(count2);
+        count2=count2+2;
+        odd();
+    }
+
+
     public static void main(String[] args) {
-        fun();
+       odd();
     }
 }
