d4ace78ee0908565e8cf7f67b4c73aec2da71041
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-02-04 12:09:38 +0530

    04/02/2026

--- two_pointer/max_consec_subarray.java
+++ two_pointer/max_consec_subarray.java
@@ -2,7 +2,7 @@ package two_pointer;
 
 public class max_consec_subarray {
     public static void main(String[] args) {
-        int arr[]={1,1,0,1,1,1},count=0,maxcount=0;
+        int arr[]={1,1,0,1,1,1},count=0,maxcount=0;;
         for(int i=0;i<arr.length;i++){
             if(arr[i]==1){
                 count++;
