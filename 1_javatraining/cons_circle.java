fd74739701400e33122bd28d995cf22a09fd2191
kmgnanadeepak <kmgnanadeepak@gmail.com>
2026-04-23 10:21:52 +0530

    23/4/26

---
+++ 1_javatraining/cons_circle.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class cons{
+    double radius;
+    cons(double r){
+        radius=r;
+    }
+    void display(){
+        double area=Math.PI*radius*radius;
+        System.out.println("area of circle: "+area);
+    }
+}
+class cons1{
+    int areaR;
+    int length;
+    int width;
+    cons1(int l,int w){
+        length=l;
+        width=w;
+    }
+    void display(){
+        int area=length*width;
+        System.out.println("area od rectangle : "+area);
+    }
+}
+class cons_circle{
+    public static void main(String a[]){
+        cons circle=new cons(4);
+        cons1 rectangle=new cons1(5,6);
+        circle.display();
+        rectangle.display();
+    }
+}
